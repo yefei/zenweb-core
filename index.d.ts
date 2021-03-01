589d88271064f4f091a796a07b1bd68cd08703c0
YeFei <316606233@qq.com>
2021-03-01 09:53:03 +0800

    1.0.0

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+import * as Koa from 'koa';
+
+export type setupCallback = (core: Core, options?: any) => Promise<void>;
+
+export declare class Core {
+  constructor();
+  koa: Koa;
+  loaded: string[];
+  defineContextCacheProperty(prop: string | number | symbol, get: (ctx: Koa.Context) => any): void;
+  check(mod: string): Core;
+  setup(mod: string | setupCallback, options?: any, name?: string): Core;
+  boot(): Promise<void>;
+  listen(port?: number): void;
+  start(port?: number): void;
+}
+
+declare module 'koa' {
+  interface BaseContext {
+    core: Core;
+  }
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const Core = require('./lib/core');
+
+module.exports = {
+  Core,
+};
