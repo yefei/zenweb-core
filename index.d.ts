c5d4106a2c7ae601aff66e5b400983a32c87c3a0
YeFei <316606233@qq.com>
2021-04-22 09:50:25 +0800

    CoreOptions

--- index.d.ts
+++ index.d.ts
@@ -2,8 +2,41 @@ import * as Koa from 'koa';
 
 export type setupCallback = (core: Core, options?: any) => Promise<void>;
 
+export interface CoreOptions {
+  /**
+   * Environment
+   * default: development
+   */
+  env?: string;
+
+  /**
+   * Signed cookie keys
+   */
+  keys?: string[];
+
+  /**
+   * Trust proxy headers
+   */
+  proxy?: boolean;
+
+  /**
+   * Subdomain offset
+   */
+  subdomainOffset?: number;
+
+  /**
+   * proxy ip header, default to X-Forwarded-For
+   */
+  proxyIpHeader?: boolean;
+
+  /**
+   * max ips read from proxy ip header, default to 0 (means infinity)
+   */
+  maxIpsCount?: boolean;
+}
+
 export declare class Core {
-  constructor();
+  constructor(options?: CoreOptions);
   koa: Koa;
   loaded: string[];
   defineContextCacheProperty(prop: string | number | symbol, get: (ctx: Koa.Context) => any): void;
